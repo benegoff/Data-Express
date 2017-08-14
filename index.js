a501dc510c0c2c906773bc7c0a0364fb88eb4a65
Ben Goff <benegoff@gmail.com>
2017-08-14 08:18:04 -0600

    Added logic to index.js

--- index.js
+++ index.js
@@ -8,4 +8,23 @@ var bluebird = require('mongodb-bluebird');
 var path = require('path');
 
 var app = express();
+
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+var urlencodedParser = bodyParser.urlencoded({
+  extended: true
+})
+
+app.get('/', route.index);
+app.get('/create', route.create);
+app.get('/edit/:id', route.edit);
+app.get('/details/:id', route.details);
+app.post('/create', urlencodedParser, route.createPerson);
+app.post('/edit/:id', urlencodedParser, route.editPerson);
+app.get('/delete/:id', route.delete);
+
 app.listen(3000);
